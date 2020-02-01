441881aeecd3edabea90aa82d1aca2151d4c343a
chanshunli <chanshunli@gmail.com>
2020-02-01 16:21:17 +0800

    Finish first setting

--- init.el
+++ init.el
@@ -6,13 +6,15 @@
         ("melpa-stable" . "http://elpa.emacs-china.org/melpa-stable/")))
 
 (setq package-selected-packages
-      '(helm
+      '(;;helm
+	ivy
 	cider
 	clojure-mode
 	smartparens
 	projectile
 	company
-	helm-projectile
+	;;helm-projectile
+	counsel-projectile
 	monokai-theme))
 
 (package-initialize)
@@ -25,3 +27,92 @@
              (not (package-installed-p package)))
     (package-install package t)))
 
+;; my setting
+(load-theme 'monokai t)
+
+(windmove-default-keybindings)
+
+(global-company-mode 1)
+
+;;(helm-mode 1)
+(ivy-mode 1)
+
+(counsel-projectile-mode 1)
+
+;;(require 'helm-config)
+
+;;(helm-projectile-on)
+
+;;(define-key global-map (kbd "C-p") 'helm-projectile-find-file)
+
+;; global:
+;; Better Defaults
+(setq-default
+ inhibit-x-resources t
+ ;; inhibit-splash-screen t
+ ;; inhibit-startup-screen t
+ initial-scratch-message ""
+ ;; Don't highlight line when buffer is inactive
+ hl-line-sticky-flag nil
+ ;; Prefer horizental split
+ split-height-threshold nil
+ split-width-threshold 120
+ ;; Don't create lockfiles
+ create-lockfiles nil
+ ;; UTF-8
+ buffer-file-coding-system 'utf-8-unix
+ default-file-name-coding-system 'utf-8-unix
+ default-keyboard-coding-system 'utf-8-unix
+ default-process-coding-system '(utf-8-unix . utf-8-unix)
+ default-sendmail-coding-system 'utf-8-unix
+ default-terminal-coding-system 'utf-8-unix
+ ;; Add final newline
+ require-final-newline t
+ ;; Larger GC threshold
+ gc-cons-threshold (* 100 1024 1024)
+ ;; Backup setups
+ backup-directory-alist `((".*" . ,temporary-file-directory))
+ auto-save-file-name-transforms `((".*" ,temporary-file-directory t))
+ backup-by-copying t
+ delete-old-versions t
+ kept-new-versions 6
+ kept-old-versions 2
+ version-control t
+ ;; Xref no prompt
+ xref-prompt-for-identifier nil
+ ;; Mouse yank at point instead of click position.
+ mouse-yank-at-point t
+ ;; This fix the cursor movement lag
+ auto-window-vscroll nil
+ ;; Don't wait for keystrokes display
+ echo-keystrokes 0
+ show-paren-style 'parenthese
+ ;; Overline no margin
+ overline-margin 0
+ tab-width 4
+ ;; Don't show cursor in non selected window.
+ cursor-in-non-selected-windows nil
+ comment-empty-lines t)
+
+(setq-default indent-tabs-mode nil)
+
+(prefer-coding-system 'utf-8)
+
+;; Show matched parens
+(setq show-paren-delay 0.01)
+(show-paren-mode 1)
+
+;; Always use dir-locals.
+(defun safe-local-variable-p (sym val) t)
+
+;; Auto revert when file change.
+(global-auto-revert-mode 1)
+
+;; Delete trailing whitespace on save.
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+
+;; Custom file path
+;; Actually we don't need custom file, this file can be generated
+;; accidentally, so we add this file to .gitignore and never load it.
+(setq custom-file "~/.emacs.d/custom.el")
