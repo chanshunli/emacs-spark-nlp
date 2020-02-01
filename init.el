8781ad17553d2db45113d77d50846ac97a7a6442
chanshunli <chanshunli@gmail.com>
2020-02-01 15:58:22 +0800

    clear and init

---
+++ init.el
@@ -0,0 +1,27 @@
+(require 'package)
+
+(setq package-archives
+      '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
+        ("melpa" . "http://elpa.emacs-china.org/melpa/")
+        ("melpa-stable" . "http://elpa.emacs-china.org/melpa-stable/")))
+
+(setq package-selected-packages
+      '(helm
+	cider
+	clojure-mode
+	smartparens
+	projectile
+	company
+	helm-projectile
+	monokai-theme))
+
+(package-initialize)
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(dolist (package package-selected-packages)
+  (when (and (assq package package-archive-contents)
+             (not (package-installed-p package)))
+    (package-install package t)))
+
