ecd19b49431e5953ce1e8fb8bd1471948725f6a5
chanshunli <chanshunli@gmail.com>
2020-02-19 13:30:14 +0800

    完成mysql的增减字段的sql生成

--- elisp/jim-scaffold.el
+++ elisp/jim-scaffold.el
@@ -3,6 +3,7 @@
 
 ;; (vc-root-dir) ;;=> 获取当前的git的root在哪里
 
+;; ------------ mysql 的 增减字段的sql 生成 --------------- start ---
 (defun get-sql-files ()
   (->
    (shell-command-to-string
@@ -36,8 +37,8 @@
 (defun generate-sql-files (table column)
   (let* ((file-name-fn
           (lambda (up-or-down)
-            (format "%smigrations/0%s-%s-%s-%s.%s.sql"
-                    (vc-root-dir)
+            (format "migrations/0%s-%s-%s-%s.%s.sql"
+                    ;; (vc-root-dir)
                     (+  (get-last-sql-number) 1)
                     table
                     (cond ((string= up-or-down "add") "add")
@@ -48,5 +49,20 @@
          (up-sql-file-name (funcall file-name-fn "add"))
          (down-sql-file-name (funcall file-name-fn "remove")))
     (list up-sql-file-name down-sql-file-name)))
+
+;; 坚持去lambda化简单的已知的定点纯函数, 最后组合函数,就能把复杂的事情做出来
+;; (generate-alter-sql-file "users" "picture")
+;; 在项目root目录下执行: M-x generate-alter-sql-file # 输入两次参数`Table:` & `Column:`
+(defun generate-alter-sql-file (table column)
+  (interactive "sTable:\nsColumn:")
+  (dolist (file (generate-sql-files table column))
+    (progn
+      (message (format  "Generate alter sql file %s" file))
+      (write-region
+       (cond ((string-match "\\(.*\\).up.sql" file) (alter-table-add-column-tpl table column))
+             ((string-match "\\(.*\\).down.sql" file) (alter-table-remove-column-tpl table column)))
+       nil file))))
+;; ------------ mysql 的 增减字段的sql 生成 --------------- end -------
+
 
 (provide 'jim-scaffold)
