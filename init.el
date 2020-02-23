3fe585882e6881987fce73cd434a6d21efb5496a
chanshunli <chanshunli@gmail.com>
2020-02-23 09:06:01 +0800

    适用于非lispy的情况的括号跳转: %

--- init.el
+++ init.el
@@ -341,3 +341,15 @@
 ;; TODO: 写很多Clojure的宏出来,写Elisp就像写clojure一样
 ;; https://github.com/plexus/a.el ;;
 ;; (a-get (a-list :foo 5 :bar 6) :foo) ;;=> 5
+
+
+;; 适用于非lispy的情况的括号跳转: %
+(defun match-paren (arg)
+  "Go to the matching paren if on a paren; otherwise insert %."
+  (interactive "p")
+  (cond
+   ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+   ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+   (t (self-insert-command (or arg 1)))))
+
+(global-set-key "%" 'match-paren)
