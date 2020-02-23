d84b0fff3d66f9efbc8829e0014233c9af9d17b8
chanshunli <chanshunli@gmail.com>
2020-02-23 22:30:32 +0800

    因为web-mode报错打不开,所以use-package的键位,没有生效

--- elisp/jim-emmet.el
+++ elisp/jim-emmet.el
@@ -5,19 +5,31 @@
 ;;(add-hook 'emmet-mode-hook (lambda () (setq emmet-indent-after-insert nil)))
 ;;(add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2))) ;; indent 2 spaces.
 
+;;  不知道为什么这个use-package的东西没有生效: 是因为web-mode打不开?的错误
 (use-package emmet-mode
-  :hook (mhtml-mode nxml-mode)
+  :hook (mhtml-mode nxml-mode css-mode)
   :bind
   (:map emmet-mode-keymap
-        ("C-RET" . 'emmet-expand-line))
+        ;; ("C-c e" . 'emmet-expand-line)
+        ("M-RET" . 'emmet-expand-line)
+        ) ;; 没有生效
   :init
   (add-hook 'mhtml-mode-hook 'emmet-mode)
-  (add-hook 'web-mode-hook 'emmet-mode))
+  ;; (add-hook 'web-mode-hook 'emmet-mode)
+  )
+
+(add-hook 'mhtml-mode-hook 'emmet-mode)
+;; (add-hook 'web-mode-hook 'emmet-mode)
 
 (add-hook 'mhtml-mode-hook 'smartparens-mode)
 
+;; (global-set-key "M-RET" 'emmet-expand-line) ;;=> 报错
+;; Debugger entered--Lisp error: (error "Key sequence C - R E T starts with non-prefix key ...")
+
+;; (global-set-key "C-c e" 'emmet-expand-line)
+
 (use-package xml
-  :mode ("\\.wxml\\'" . web-mode))
+  :mode ("\\.wxml\\'" . mhtml-mode))
 
 (use-package css-mode
   :mode ("\\.wxss\\'" . css-mode))
