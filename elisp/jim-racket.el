8f80d19a9ac9805eeaaf62597dad66965491029b
chanshunli <chanshunli@gmail.com>
2020-05-09 10:54:56 +0800

    用S表达式来写datalog

--- elisp/jim-racket.el
+++ elisp/jim-racket.el
@@ -1,2 +1,14 @@
 
+
+(defun rkt-datalog-head ()
+  "一个问题在某个领域很难,但是跳出这个领域,变成另外一个领域很简单的问题:https://docs.racket-lang.org/datalog/"
+  (interactive)
+  (insert "#lang datalog
+(racket/base).\n"))
+
+(defun rkt-datalog-sexp ()
+  "用S表达式来写datalog: https://docs.racket-lang.org/datalog/Parenthetical_Datalog_Module_Language.html"
+  (interactive)
+  (insert "#lang datalog/sexp\n"))
+
 (provide 'jim-racket)
