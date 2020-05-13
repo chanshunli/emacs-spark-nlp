a340bf516e2da1c262250b122ecf79496a399098
chanshunli <chanshunli@gmail.com>
2020-05-13 10:44:45 +0800

    TODO: snippets可以写在一个文件里面,然后每次启动的时候,就去生成一堆的模板文件

--- elisp/jim-yasnipet.el
+++ elisp/jim-yasnipet.el
@@ -15,4 +15,10 @@
 ;; 如果有projectile的话，这个能自动把 project root/.yas/ 里面的模板加进来, 比如 .yas/clojure-mode/xxx
 (add-hook 'yas-minor-mode-hook #'user/yas-load-local-snippets)
 
+
+;; TODO:
+;; 1. snippets可以写在一个文件里面,然后每次启动的时候,就去生成一堆的模板文件
+;; 2. 直接搜索所有的snippets的的指令的方法
+;; for file in  `find ~/.emacs.d/snippets/ -name "*" `; do echo "\n###====$file=====\n"; cat $file 2>/dev/null; done
+
 (provide 'jim-yasnipet)
