68710047372fb482bc2018c4b80931bb31a21907
chanshunli <chanshunli@gmail.com>
2020-06-03 20:30:46 +0800

    TODO: 改成cider-eval,填入参数去全文搜索

--- elisp/jim-datalog.el
+++ elisp/jim-datalog.el
@@ -9,4 +9,36 @@
 ")
   )
 
+(defun dl/pull ()
+  (interactive)
+  (insert
+   "
+(d/pull db '[*] eid)
+"
+   )
+  )
+
+(defun dl/pull1 ()
+  (interactive)
+  (insert
+   "
+(d/pull db ' led-zeppelin)
+"
+   )
+  )
+
+;; TODO: 改成cider-eval,填入参数去全文搜索
+(defun dl/search ()
+  "全文搜索内容"
+  (interactive)
+  (insert
+   "
+(d/q '[:find ?entity ?name ?tx ?score
+         :in $ ?search
+         :where [(fulltext $ :artist/name ?search) [[?entity ?name ?tx ?score]]]]
+    db \"zeppelin\")
+"
+   )
+  )
+
 (provide 'jim-datalog)
